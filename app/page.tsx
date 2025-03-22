8a5877d9687395623ce0d2d3bf5113c8cf8eef2a
HalaMadi <Halamadi75@gmail.com>
2025-03-23 01:20:58 +0200

    Implement responsive navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-            <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-                <Image
-                    src="/jerry.jpeg"
-                    alt="Next.js logo"
-                    width={650}
-                    height={38}
-                    className="rounded-2xl"
-                />
-                <div className="text-center">
-                    <p className="pb-6 text-xl">
-                        Hello, wonderful team. I hope you are all well.😄 <br />{" "}
-                        Get to work and remember that no matter how stressful it
-                        is, it will pass. 🎉 <br /> Don&apos;t hesitate to ask
-                        for support at any time.🚀
-                    </p>
-                    <span className="mt-6 text-2xl font-bold text-rose-400 sm:text-left sm:text-3xl">
-                        Your awesome leader, Hala! 😎
-                    </span>
-                </div>
-            </main>
+        <div className="">
+          <Navbar/>
+           
         </div>
     );
 }
